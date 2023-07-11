344b4f54f6ea1f325c850586b2c7464a500c81b3
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2023-07-11 12:48:34 -0500

    setting test env

---
+++ e2e/app.e2e.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+
+const createApp = require('./../src/app');
+
+describe('test for app', () => {
+  let app = null;
+  let server = null;
+  let api = null;
+
+  beforeEach(() => {
+    app = createApp();
+    server = app.listen(9000);
+    api = request(app);
+  });
+
+  test('GET /hello', async () => {
+    const response = await api.get('/hello');
+    expect(response).toBeTruthy();
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.name).toEqual('sergio');
+    expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+});
