c2294849014652b3b6b2381d24f6f227ce2a5a30
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2023-07-11 17:13:38 -0500

    test for get user

--- e2e/users.e2e.js
+++ e2e/users.e2e.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-
 const createApp = require('./../src/app');
+const { models } = require('./../src/db/sequelize');
 
 describe('test for /users path', () => {
   let app = null;
@@ -13,8 +13,14 @@ describe('test for /users path', () => {
     api = request(app);
   });
 
-  describe('GET /users', () => {
-    // test for /users
+  describe('GET /users/{id}', () => {
+    test('should return a user', async () => {
+      const user = await models.User.findByPk('1');
+      const { statusCode, body } = await api.get(`/api/v1/users/${user.id}`);
+      expect(statusCode).toEqual(200);
+      expect(body.id).toEqual(user.id);
+      expect(body.email).toEqual(user.email);
+    });
   });
 
   describe('POST /users', () => {
@@ -34,6 +40,8 @@ describe('test for /users path', () => {
       expect(response.body.message).toMatch(/email/);
     });
   });
+
+  // TODO: test with valid data
 
   describe('PUT /users', () => {
     // test for /users
