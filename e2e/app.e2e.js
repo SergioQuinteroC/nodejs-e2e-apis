d6625b22ca215f4203ea472f6e706cd378d8a7ba
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2023-07-11 17:47:42 -0500

    test endpoint with apikey

--- e2e/app.e2e.js
+++ e2e/app.e2e.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
-
 const createApp = require('./../src/app');
+const { config } = require('./../src/config/config');
 
 describe('test for app', () => {
   let app = null;
@@ -19,6 +19,27 @@ describe('test for app', () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body.name).toEqual('sergio');
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  describe('GET /nueva-ruta', () => {
+    test('should return 401', async () => {
+      const { statusCode } = await api.get('/nueva-ruta');
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return 401 with invalid apiKey', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: '1212',
+      });
+      expect(statusCode).toEqual(401);
+    });
+
+    test('should return 200', async () => {
+      const { statusCode } = await api.get('/nueva-ruta').set({
+        api: config.apiKey,
+      });
+      expect(statusCode).toEqual(200);
+    });
   });
 
   afterAll(() => {
